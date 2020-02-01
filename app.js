0da0c73153bb19eac5ad51c986255d58074bb611
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-02-01 13:14:29 +0530

    Restructured express

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const morgan = require('morgan');
 const helmet = require('helmet');
-const Joi = require('joi');
 const express = require('express');
-const logger = require('./logger');
+const logger = require('./middleware/logger');
+const members = require('./routes/members');
+const home = require('./routes/home');
 const app = express();
 
 app.set('view engine', 'pug');
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+app.use('/members', members);
+app.use('/', home);
 
 // NODE_ENV=production npm test
 if (app.get('env') === 'development') {
@@ -20,77 +23,6 @@ if (app.get('env') === 'development') {
 }
 
 app.use(logger);
-
-const members = [
-  { id: 1, name: 'Sameer', age: 20 },
-  { id: 2, name: 'Anshu', age: 19 },
-  { id: 3, name: 'Anjali', age: 23 },
-  { id: 4, name: 'Sumit', age: 18 },
-  { id: 5, name: 'Manisha', age: 24 }
-];
-app.get('/', (req, res) => {
-  res.render('index', { title: 'My Express App', message: 'Hello World' });
-});
-
-app.get('/members', (req, res) => {
-  res.send(members);
-});
-
-app.post('/members', (req, res) => {
-  const { error } = validateMember(req.body);
-
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const member = {
-    id: members.length + 1,
-    name: req.body.name,
-    age: req.body.age
-  };
-  members.push(member);
-  res.send(member);
-});
-
-app.get('/member/:id', (req, res) => {
-  const member = members.find(c => c.id === parseInt(req.params.id));
-
-  if (!member) return res.status(404).send('There is no member with given ID');
-
-  res.send(member);
-});
-
-app.put('/member/:id', (req, res) => {
-  const member = members.find(c => c.id === parseInt(req.params.id));
-
-  if (!member) return res.status(404).send('There is no member with given ID');
-
-  const { error } = validateMember(req.body);
-
-  if (error) return res.status(400).send(error.details[0].message);
-
-  member.name = req.body.name;
-  res.send(member);
-});
-
-app.delete('/member/:id', (req, res) => {
-  const member = members.find(c => c.id === parseInt(req.params.id));
-
-  if (!member) return res.status(404).send('There is no member with given ID');
-
-  const index = members.indexOf(member);
-  members.splice(index, 1);
-  res.send(member);
-});
-
-function validateMember(member) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required(),
-    age: Joi.number().required()
-  };
-
-  return Joi.validate(member, schema);
-}
 
 // PORT=5000 node app.js
 const port = process.env.PORT || 3000;
---
+++ routes/members.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+
+const members = [
+  { id: 1, name: 'Sameer', age: 20 },
+  { id: 2, name: 'Anshu', age: 19 },
+  { id: 3, name: 'Anjali', age: 23 },
+  { id: 4, name: 'Sumit', age: 18 },
+  { id: 5, name: 'Manisha', age: 24 }
+];
+
+router.get('/', (req, res) => {
+  res.send(members);
+});
+
+router.post('/', (req, res) => {
+  const { error } = validateMember(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const member = {
+    id: members.length + 1,
+    name: req.body.name,
+    age: req.body.age
+  };
+  members.push(member);
+  res.send(member);
+});
+
+router.get('/:id', (req, res) => {
+  const member = members.find(c => c.id === parseInt(req.params.id));
+
+  if (!member) return res.status(404).send('There is no member with given ID');
+
+  res.send(member);
+});
+
+router.put('/:id', (req, res) => {
+  const member = members.find(c => c.id === parseInt(req.params.id));
+
+  if (!member) return res.status(404).send('There is no member with given ID');
+
+  const { error } = validateMember(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+
+  member.name = req.body.name;
+  member.age = req.body.age;
+  res.send(member);
+});
+
+router.delete('/:id', (req, res) => {
+  const member = members.find(c => c.id === parseInt(req.params.id));
+
+  if (!member) return res.status(404).send('There is no member with given ID');
+
+  const index = members.indexOf(member);
+  members.splice(index, 1);
+  res.send(member);
+});
+
+function validateMember(member) {
+  const schema = {
+    name: Joi.string()
+      .min(3)
+      .required(),
+    age: Joi.number().required()
+  };
+
+  return Joi.validate(member, schema);
+}
+
+module.exports = router;
