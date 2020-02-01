5e93c944b256cacc777d974f266b027d025d619b
Saurabh Patel <38186468+Saurabhrkp@users.noreply.github.com>
2020-02-01 11:57:22 +0530

    removed extra

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const express = require('express');
 const logger = require('./logger');
 const app = express();
 
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`app: ${app.get('env')}`);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
